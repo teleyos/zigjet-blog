aea3e2c10910f2d9fc7e75511f9a2cda519f9b97
teleios <tristanduret@hotmail.Fr>
2025-01-07 20:40:25 +0100

    bug fix + discovering bundle

--- src/main.zig
+++ src/main.zig
@@ -87,7 +87,7 @@ pub const jetzig_options = struct {
             .path = "/",
         },
         .production => .{
-            .same_site = true,
+            .same_site = .strict,
             .secure = true,
             .http_only = true,
             .path = "/",
